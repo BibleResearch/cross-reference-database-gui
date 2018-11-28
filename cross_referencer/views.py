d39903f6d5472b32d51d2acc3233fd662c4db9a7
Floyd Hightower <floyd.hightower27@gmail.com>
2018-11-28 07:46:01 -0300

    Improving update process to be able to change start and destination references

--- cross_referencer/views.py
+++ cross_referencer/views.py
@@ -5,7 +5,7 @@ from django.views.generic.list import ListView
 from django.http import HttpResponseRedirect
 
 from .models import CrossReference
-from .utility import create_bible_reference
+from .utility import create_bible_reference, normalize_bible_passage
 
 
 class CrossReferenceListView(ListView):
@@ -24,7 +24,6 @@ class CrossReferenceCreateView(CreateView):
             return HttpResponseRedirect(reverse('cross_referencer:list'))
 
     def save(self, posted_data):
-        # TODO: add normalization of passages here...
         starting_reference = create_bible_reference(posted_data.get('startingPassage'))
         destination_reference = create_bible_reference(posted_data.get('destinationPassage'))
 
@@ -48,7 +47,39 @@ class CrossReferenceDetailView(DetailView):
 class CrossReferenceEditView(UpdateView):
     model = CrossReference
     fields = ['classification', 'explanation', 'sources']
-    success_url = reverse_lazy('cross_referencer:list')
+    template_name_suffix = '_update_form'
+
+    def get_success_url(self, **kwargs):
+        return reverse('cross_referencer:details', args=(self.object.id,))
+
+    def post(self, request, **kwargs):
+        return self.save(request.POST, kwargs['pk'])
+
+    def save(self, posted_data, object_id):
+        # get all of the fields from the posted data
+        starting_reference = posted_data.get('startingPassage')
+        destination_reference = posted_data.get('destinationPassage')
+        classification = posted_data.get('classification')
+        explanation = posted_data.get('explanation')
+        sources = posted_data.get('sources')
+
+        # get a queryset with the current object - I am intentionally getting queryset rather than the object itself so that I can use the `update` function below
+        current_object = CrossReference.objects.filter(pk=object_id)
+
+        # update the metadata for the cross reference
+        current_object.update(classification=classification, explanation=explanation, sources=sources)
+
+        # if the start of the cross reference has changed, update it
+        if normalize_bible_passage(starting_reference) != current_object[0].start:
+            new_start = create_bible_reference(starting_reference)
+            current_object.update(start=new_start)
+
+        # if the destination of the cross reference has changed, update it
+        if normalize_bible_passage(destination_reference) != current_object[0].destination:
+            new_destination = create_bible_reference(destination_reference)
+            current_object.update(destination=new_destination)
+
+        return HttpResponseRedirect(reverse('cross_referencer:details', args=(current_object[0].id,)))
 
 
 class CrossReferenceDeleteView(DeleteView):
